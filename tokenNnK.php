fa79d63ffd41c72c64baed7f162768e97c6bd407
Harits Naufal <haritsnaufalichs11@gmail.com>
2022-06-18 15:19:49 +0700

    Edit Some Codes

--- tokenNnK.php
+++ tokenNnK.php
@@ -29,39 +29,39 @@ function getExchangeNKA($Query) {
 }
 
 // NKT Section
-echo PHP_EOL . '[+] Token   : ' . 'NKT' . PHP_EOL;
-echo '[+] Price   : ' . getNKT('priceMajor') . '.' . substr(getNKT('priceMinor'), 0, 4) . ' (USD)' . PHP_EOL;
+echo PHP_EOL . ' [+] Token   : ' . 'NKT' . PHP_EOL;
+echo ' [+] Price   : ' . getNKT('priceMajor') . '.' . substr(getNKT('priceMinor'), 0, 4) . ' (USD)' . PHP_EOL;
 if (getNKT('percentMajor') >= 0) {
-  echo "[+] Change  : \033[0;32m" . getNKT('percentMajor') . '.' . substr(getNKT('percentMinor'), 0, 2) . '%' . "\033[0m" . ' (+)' . PHP_EOL;
+  echo " [+] Change  : \033[0;32m" . getNKT('percentMajor') . '.' . substr(getNKT('percentMinor'), 0, 2) . '%' . "\033[0m" . ' (+)' . PHP_EOL;
 } else {
-  echo "[+] Change  : \033[0;31m" . abs(getNKT('percentMajor') . '.' . substr(getNKT('percentMinor'), 0, 2)) . '%' . "\033[0m" . ' (-)' . PHP_EOL;
+  echo " [+] Change  : \033[0;31m" . abs(getNKT('percentMajor') . '.' . substr(getNKT('percentMinor'), 0, 2)) . '%' . "\033[0m" . ' (-)' . PHP_EOL;
 }
 if (getExchangeNKT('increaseExchangeRate') > 0) {
-  echo '[+] Rate    : ' . getExchangeNKT('exchangeRate') . ' (' . "\033[0;32m" . getExchangeNKT('increaseExchangeRate') . "\033[0m" . ')' . PHP_EOL;
+  echo ' [+] Rate    : ' . getExchangeNKT('exchangeRate') . ' (' . "\033[0;32m" . getExchangeNKT('increaseExchangeRate') . "\033[0m" . ')' . PHP_EOL;
 } else if (getExchangeNKT('increaseExchangeRate') == 0) {
-  echo '[+] Rate    : ' . getExchangeNKT('exchangeRate') . ' (' . "\033[0;36m" . abs(getExchangeNKT('increaseExchangeRate')) . "\033[0m" . ')' . PHP_EOL;
+  echo ' [+] Rate    : ' . getExchangeNKT('exchangeRate') . ' (' . "\033[0;36m" . abs(getExchangeNKT('increaseExchangeRate')) . "\033[0m" . ')' . PHP_EOL;
 } else {
-  echo '[+] Rate    : ' . getExchangeNKT('exchangeRate') . ' (' . "\033[0;31m" . abs(getExchangeNKT('increaseExchangeRate')) . "\033[0m" . ')' . PHP_EOL;
+  echo ' [+] Rate    : ' . getExchangeNKT('exchangeRate') . ' (' . "\033[0;31m" . abs(getExchangeNKT('increaseExchangeRate')) . "\033[0m" . ')' . PHP_EOL;
 }
-echo '[+] Last Updated on ' . date('F j Y, g:i', substr(getNKT('lastPriceUpdated'), 0, 10)) . PHP_EOL;
+echo ' [+] Last Updated on ' . date('F j Y, g:i', substr(getNKT('lastPriceUpdated'), 0, 10)) . PHP_EOL;
 // echo '[+] Last Updated on ' . date('F j Y, g:i', getNKT('lastPriceUpdated')) . PHP_EOL;
 
 // NKA Section
-echo PHP_EOL . '[+] Token   : ' . 'NKA' . PHP_EOL;
-echo '[+] Price   : ' . getNKA('priceMajor') . '.' . substr(getNKA('priceMinor'), 0, 4) . ' (USD)' . PHP_EOL;
+echo PHP_EOL . ' [+] Token   : ' . 'NKA' . PHP_EOL;
+echo ' [+] Price   : ' . getNKA('priceMajor') . '.' . substr(getNKA('priceMinor'), 0, 4) . ' (USD)' . PHP_EOL;
 if (getNKA('percentMajor') >= 0) {
-  echo "[+] Change  : \033[0;32m" . getNKA('percentMajor') . '.' . substr(getNKA('percentMinor'), 0, 2) . '%' . "\033[0m" . ' (+)' . PHP_EOL;
+  echo " [+] Change  : \033[0;32m" . getNKA('percentMajor') . '.' . substr(getNKA('percentMinor'), 0, 2) . '%' . "\033[0m" . ' (+)' . PHP_EOL;
 } else {
-  echo "[+] Change  : \033[0;31m" . abs(getNKA('percentMajor') . '.' . substr(getNKA('percentMinor'), 0, 2)) . '%' . "\033[0m" . ' (-)' . PHP_EOL;
+  echo " [+] Change  : \033[0;31m" . abs(getNKA('percentMajor') . '.' . substr(getNKA('percentMinor'), 0, 2)) . '%' . "\033[0m" . ' (-)' . PHP_EOL;
 }
 if (getExchangeNKA('increaseExchangeRate') > 0) {
-  echo '[+] Rate    : ' . getExchangeNKA('exchangeRate') . ' (' . "\033[0;32m" . getExchangeNKA('increaseExchangeRate') . "\033[0m" . ')' . PHP_EOL;
+  echo ' [+] Rate    : ' . getExchangeNKA('exchangeRate') . ' (' . "\033[0;32m" . getExchangeNKA('increaseExchangeRate') . "\033[0m" . ')' . PHP_EOL;
 } else if (getExchangeNKA('increaseExchangeRate') == 0) {
-  echo '[+] Rate    : ' . getExchangeNKA('exchangeRate') . ' (' . "\033[0;36m" . abs(getExchangeNKA('increaseExchangeRate')) . "\033[0m" . ')' . PHP_EOL;
+  echo ' [+] Rate    : ' . getExchangeNKA('exchangeRate') . ' (' . "\033[0;36m" . abs(getExchangeNKA('increaseExchangeRate')) . "\033[0m" . ')' . PHP_EOL;
 } else {
-  echo '[+] Rate    : ' . getExchangeNKA('exchangeRate') . ' (' . "\033[0;31m" . abs(getExchangeNKA('increaseExchangeRate')) . "\033[0m" . ')' . PHP_EOL;
+  echo ' [+] Rate    : ' . getExchangeNKA('exchangeRate') . ' (' . "\033[0;31m" . abs(getExchangeNKA('increaseExchangeRate')) . "\033[0m" . ')' . PHP_EOL;
 }
-echo '[+] Last Updated on ' . date('F j Y, g:i', substr(getNKA('lastPriceUpdated'), 0, 10)) . PHP_EOL;
+echo ' [+] Last Updated on ' . date('F j Y, g:i', substr(getNKA('lastPriceUpdated'), 0, 10)) . PHP_EOL;
 // echo '[+] Last Updated on ' . date('F j Y, g:i', getNKA('lastPriceUpdated')) . PHP_EOL;
 
 ?>
