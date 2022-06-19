9d62031d7f59ba757115c0ed9e1d6fd50582fbb8
Harits Naufal <haritsnaufalichs11@gmail.com>
2022-06-19 15:02:01 +0700

    Edit Logics, Python

--- tokenNnK.py
+++ tokenNnK.py
@@ -1,64 +1,82 @@
-from datetime import date
 from json import JSONDecoder
 import requests
-import datetime
 
+# NKT Function
 def getNKT(Query):
   NKT = s.get('https://ninokuni.marblex.io/api/price?tokenType=NKT')
   NKT = JSONDecoder().decode(NKT.text)
   return NKT['currencies']['USD'][Query]
-
-def getNKA(Query):
-  NKA = s.get('https://ninokuni.marblex.io/api/price?tokenType=NKA')
-  NKA = JSONDecoder().decode(NKA.text)
-  return NKA['currencies']['USD'][Query]
-
 def getExchangeNKT(Query):
   eNKT = s.get('https://ninokuni.marblex.io/api/exchangeRate?tokenType=NKT')
   eNKT = JSONDecoder().decode(eNKT.text)
   Last = len(eNKT['result']) - 1
   return eNKT['result'][Last][Query]
 
+# NKA Function
+def getNKA(Query):
+  NKA = s.get('https://ninokuni.marblex.io/api/price?tokenType=NKA')
+  NKA = JSONDecoder().decode(NKA.text)
+  return NKA['currencies']['USD'][Query]
 def getExchangeNKA(Query):
   eNKA = s.get('https://ninokuni.marblex.io/api/exchangeRate?tokenType=NKA')
   eNKA = JSONDecoder().decode(eNKA.text)
   Last = len(eNKA['result']) - 1
   return eNKA['result'][Last][Query]
 
+# NKT Print Function
 def printNKT():
-  print('\n [+] Token  : NKT')
-  print(' [+] Price  : ' + str(getNKT('priceMajor')) + '.' + str(getNKT('priceMinor')[0:4]) + ' USD')
-  if (int(getNKT('percentMajor')) >= 0):
-    print(' [+] Change : ' + '\033[92m' + str(getNKT('percentMajor')) + '.' + str(getNKT('percentMinor')[0:2]) + '%' + '\033[0m' + ' (+)')
+  print('\n [+] Token Name    : NKT')
+  print(' [+] Live Price    : ' + liveNKTPrice + ' USD')
+  print(' [+] Last Price    : ' + lastNKTPrice + ' USD' + ' (Previous Close Price)')
+  if (liveNKTPrice > lastNKTPrice):
+    print(' [+] Changes       : ' + '\033[92m' + str(abs(int(getNKT('percentMajor')))) + '.' + str(getNKT('percentMinor')[0:2]) + '%' + '\033[0m' + ' (+)')
   else:
-    print(' [+] Change : ' + '\033[91m' + str(abs(int(getNKT('percentMajor')))) + '.' + str(getNKT('percentMinor')[0:2]) + '%' + '\033[0m' + ' (-)')
+    print(' [+] Changes       : ' + '\033[91m' + str(abs(int(getNKT('percentMajor')))) + '.' + str(getNKT('percentMinor')[0:2]) + '%' + '\033[0m' + ' (-)')
   if (int(getExchangeNKT('increaseExchangeRate')) > 0):
-    print(' [+] Rate   : ' + str(getExchangeNKT('exchangeRate')) + ' (' + '\033[92m' + str(getExchangeNKT('increaseExchangeRate')) + '\033[0m' + ')')
+    print(' [+] Exchange Rate : ' + str(getExchangeNKT('exchangeRate')) + ' (' + '\033[92m' + str(getExchangeNKT('increaseExchangeRate')) + '\033[0m' + ')')
   elif (int(getExchangeNKT('increaseExchangeRate')) == 0):
-    print(' [+] Rate   : ' + str(getExchangeNKT('exchangeRate')) + ' (' + '\033[96m' + str(getExchangeNKT('increaseExchangeRate')) + '\033[0m' + ')')
+    print(' [+] Exchange Rate : ' + str(getExchangeNKT('exchangeRate')) + ' (' + '\033[96m' + str(getExchangeNKT('increaseExchangeRate')) + '\033[0m' + ')')
   else:
-    print(' [+] Rate   : ' + str(getExchangeNKT('exchangeRate')) + ' (' + '\033[91m' + str(abs(int(getExchangeNKT('increaseExchangeRate')))) + '\033[0m' + ')')
-  print(' [+] Last Updated on ' + date.fromtimestamp(int(getNKT('lastPriceUpdated')[0:10])).strftime('%B %d %Y'))
+    print(' [+] Exchange Rate : ' + str(getExchangeNKT('exchangeRate')) + ' (' + '\033[91m' + str(abs(int(getExchangeNKT('increaseExchangeRate')))) + '\033[0m' + ')')
 
+# NKA Print Function
 def printNKA():
-  print('\n [+] Token  : NKA')
-  print(' [+] Price  : ' + str(getNKA('priceMajor')) + '.' + str(getNKA('priceMinor')[0:4]) + ' USD')
-  if (int(getNKA('percentMajor')) >= 0):
-    print(' [+] Change : ' + '\033[92m' + str(getNKA('percentMajor')) + '.' + str(getNKA('percentMinor')[0:2]) + '%' + '\033[0m' + ' (+)')
+  print('\n [+] Token Name    : NKA')
+  print(' [+] Live Price    : ' + liveNKAPrice + ' USD')
+  print(' [+] Last Price    : ' + lastNKAPrice + ' USD' + ' (Previous Close Price)')
+  if (liveNKAPrice > lastNKAPrice):
+    print(' [+] Changes       : ' + '\033[92m' + str(abs(int(getNKA('percentMajor')))) + '.' + str(getNKA('percentMinor')[0:2]) + '%' + '\033[0m' + ' (+)')
   else:
-    print(' [+] Change : ' + '\033[91m' + str(abs(int(getNKA('percentMajor')))) + '.' + str(getNKA('percentMinor')[0:2]) + '%' + '\033[0m' + ' (-)')
+    print(' [+] Changes       : ' + '\033[91m' + str(abs(int(getNKA('percentMajor')))) + '.' + str(getNKA('percentMinor')[0:2]) + '%' + '\033[0m' + ' (-)')
   if (int(getExchangeNKA('increaseExchangeRate')) > 0):
-    print(' [+] Rate   : ' + str(getExchangeNKA('exchangeRate')) + ' (' + '\033[92m' + str(getExchangeNKA('increaseExchangeRate')) + '\033[0m' + ')')
+    print(' [+] Exchange Rate : ' + str(getExchangeNKA('exchangeRate')) + ' (' + '\033[92m' + str(getExchangeNKA('increaseExchangeRate')) + '\033[0m' + ')')
   elif (int(getExchangeNKA('increaseExchangeRate')) == 0):
-    print(' [+] Rate   : ' + str(getExchangeNKA('exchangeRate')) + ' (' + '\033[96m' + str(getExchangeNKA('increaseExchangeRate')) + '\033[0m' + ')')
+    print(' [+] Exchange Rate : ' + str(getExchangeNKA('exchangeRate')) + ' (' + '\033[96m' + str(getExchangeNKA('increaseExchangeRate')) + '\033[0m' + ')')
   else:
-    print(' [+] Rate   : ' + str(getExchangeNKA('exchangeRate')) + ' (' + '\033[91m' + str(abs(int(getExchangeNKA('increaseExchangeRate')))) + '\033[0m' + ')')
-  print(' [+] Last Updated on ' + date.fromtimestamp(int(getNKA('lastPriceUpdated')[0:10])).strftime('%B %d %Y'))
+    print(' [+] Exchange Rate : ' + str(getExchangeNKA('exchangeRate')) + ' (' + '\033[91m' + str(abs(int(getExchangeNKA('increaseExchangeRate')))) + '\033[0m' + ')')
 
 if __name__ == '__main__':
+  # Initialize Requests Session
   s = requests.Session()
-  start = datetime.datetime.now()
+  
+  # NKT
+  # posNKTExchangeRate = str(getExchangeNKT('exchangeRate')) + ' (' + '\033[92m' + str(getExchangeNKT('increaseExchangeRate')) + '\033[0m' + ')'
+  # netNKTExchangeRate = str(getExchangeNKT('exchangeRate')) + ' (' + '\033[96m' + str(getExchangeNKT('increaseExchangeRate')) + '\033[0m' + ')'
+  # negNKTExchangeRate = str(getExchangeNKT('exchangeRate')) + ' (' + '\033[91m' + str(abs(int(getExchangeNKT('increaseExchangeRate')))) + '\033[0m' + ')'
+  liveNKTPrice = str(getNKT('priceMajor')) + '.' + str(getNKT('priceMinor')[0:4])
+  lastNKTPrice = str(getNKT('previousClosePriceMajor')) + '.' + str(getNKT('previousClosePriceMinor')[0:4])
+  # posNKTChanges = '\033[92m' + str(abs(int(getNKT('percentMajor')))) + '.' + str(getNKT('percentMinor')[0:2]) + '%' + '\033[0m' + ' (+)'
+  # negNKTChanges = '\033[91m' + str(abs(int(getNKT('percentMajor')))) + '.' + str(getNKT('percentMinor')[0:2]) + '%' + '\033[0m' + ' (-)'
+
+  # NKA
+  # posNKAExchangeRate = str(getExchangeNKA('exchangeRate')) + ' (' + '\033[92m' + str(getExchangeNKA('increaseExchangeRate')) + '\033[0m' + ')'
+  # netNKAExchangeRate = str(getExchangeNKA('exchangeRate')) + ' (' + '\033[96m' + str(getExchangeNKA('increaseExchangeRate')) + '\033[0m' + ')'
+  # negNKAExchangeRate = str(getExchangeNKA('exchangeRate')) + ' (' + '\033[91m' + str(abs(int(getExchangeNKA('increaseExchangeRate')))) + '\033[0m' + ')'
+  liveNKAPrice = str(getNKA('priceMajor')) + '.' + str(getNKA('priceMinor')[0:4])
+  lastNKAPrice = str(getNKA('previousClosePriceMajor')) + '.' + str(getNKA('previousClosePriceMinor')[0:4])
+  # posNKAChanges = '\033[92m' + str(abs(int(getNKA('percentMajor')))) + '.' + str(getNKA('percentMinor')[0:2]) + '%' + '\033[0m' + ' (+)'
+  # negNKAChanges = '\033[91m' + str(abs(int(getNKA('percentMajor')))) + '.' + str(getNKA('percentMinor')[0:2]) + '%' + '\033[0m' + ' (-)'
+
+  # Print
   printNKT()
   printNKA()
-  end = datetime.datetime.now() - start
-  print('\n [+] Execution Time : ' + str(end.seconds) + '.' + str(end.microseconds)[0:2] + ' Seconds')
